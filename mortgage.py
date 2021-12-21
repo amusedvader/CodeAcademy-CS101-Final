b766a9d9f1488b877974db60da2de5ee2f88d1a3
amusedvader <94640268+amusedvader@users.noreply.github.com>
2021-12-21 06:09:53 -0800

    Some bug fixes and added qualifiers

--- mortgage.py
+++ mortgage.py
@@ -1,17 +1,66 @@
 #!/usr/bin/env python3
 from decimal import Decimal, getcontext
+import pandas as pd
+import numpy as np
+
+dict_zip_to_county = pd.read_csv('zip_to_county.csv', header=1, index_col=0, squeeze=True).to_dict()
+
+dict_county_to_loan1 = pd.read_csv('county_1_2_3_4_unitloanlimits.csv', usecols=[0,1], index_col=0, header=1, squeeze=True).to_dict()
+dict_county_to_loan2 = pd.read_csv('county_1_2_3_4_unitloanlimits.csv', usecols=[0,2], index_col=0, header=1, squeeze=True).to_dict()
+dict_county_to_loan3 = pd.read_csv('county_1_2_3_4_unitloanlimits.csv', usecols=[0,3], index_col=0, header=1, squeeze=True).to_dict()
+dict_county_to_loan4 = pd.read_csv('county_1_2_3_4_unitloanlimits.csv', usecols=[0,4], index_col=0, header=1, squeeze=True).to_dict()
+
+for i in dict_county_to_loan1.keys():
+	dict_county_to_loan1[i] = dict_county_to_loan1[i].replace('$', '').replace(' ','').replace(',','')
+for i in dict_county_to_loan2.keys():
+	dict_county_to_loan2[i] = dict_county_to_loan2[i].replace('$', '').replace(' ','').replace(',','')
+for i in dict_county_to_loan3.keys():
+	dict_county_to_loan3[i] = dict_county_to_loan3[i].replace('$', '').replace(' ','').replace(',','')
+for i in dict_county_to_loan4.keys():
+	dict_county_to_loan4[i] = dict_county_to_loan4[i].replace('$', '').replace(' ','').replace(',','')
 
 def findPayment(loan, interest_rate, years_of_loan):
 	""" takes a loan, interest rate r and m total months of the payment"""
 	r = (interest_rate / 100) / 12
 	m = years_of_loan * 12
-	return (r * loan * ((1 + r)**m) / ((1 + r)** m - 1))
+	return (r * loan * ((1 + r)**m) / (((1 + r)** m) - 1))
 
 getcontext().prec = 5
 
 def loan_to_value(home_value, down_payment):
 	return Decimal((home_value - down_payment) / home_value)
+
+def findinterestRate(credit_score):
+	if credit_score >= 760:
+		return .0333
+	if credit_score >= 740:
+		return .033
+	if credit_score >= 720:
+		return .03405
+	if credit_score >= 700:
+		return .03616
+	if credit_score >= 680:
+		return .03625
+	if credit_score >= 660:
+		return .03625
 	
+def MI(home_value, down_payment, credit_score):
+	ltv = loan_to_value(home_value, down_payment)
+	if ltv <= .8000000000:
+		return None
+	else:
+		if credit_score >= 760:
+			return .0017
+		if credit_score >= 740:
+			return .0022
+		if credit_score >= 720:
+			return .0029
+		if credit_score >= 700:
+			return .0036
+		if credit_score >= 680:
+			return .0049
+		if credit_score >= 660:
+			return .0072
 
 class Mortgage(object):
 	def __init__(self, loan, annRate, years):
@@ -34,43 +83,29 @@ class Mortgage(object):
 	def __str__(self):
 		return self.legend
 	
-class findInterestRate(object):
-	def __init__(self, home_value, down_payment, credit_score):
-		self.cs = credit_score
-		self.ltv = loan_to_value(home_value, down_payment)
-		self.dp = down_payment
-		self.hv = home_value
-		
-	def preQualify_premrate(self):
-		if self.hv - self.dp > 914000:
-			return False
-		elif self.ltv >= .91:
-			return False
-		elif self.cs < 680:
-			return False
 		
 class calcDebt_defer(object):
-	def __init__(self, student_loans_total = 0, car_payment = 0, credit_card = 0, other = 0, gross_pay = 50000, credit_percent = 0):
+	def __init__(self, student_loans_total = 0, car_payment = 0, credit_card = 1, other = 0, gross_pay = 50000, credit_percent = 7.5, payment = 0):
 		self.slt = float(student_loans_total)
 		self.cp = float(car_payment)
 		self.cc = float(credit_card)
 		self.credp = float(credit_percent)
 		self.oth = float(other)
 		self.gp = float(gross_pay)
+		self.p = float(payment)
 		
-	
 	def debttoincomedefer(self):
-		return (max((findPayment(self.slt, 6.5, 10) + self.cp + self.cc + self.oth), 1)/(self.gp / 12)) * 100
+		return (max((findPayment(self.slt, 6.5, 10) + self.cp + findPayment(self.cc, self.credp, .83333) + self.oth + self.p), 1)/(self.gp / 12)) * 100
 		
 class calcDebt(object):
-	def __init__(self, student_loans_monthly = 0, car_payment = 0, credit_card = 0, other = 0, gross_pay = 50000, credit_percent = 0):
+	def __init__(self, student_loans_monthly = 0, car_payment = 0, credit_card = 1, other = 0, gross_pay = 50000, credit_percent = 7.5, payment = 0):
 		self.slm = float(student_loans_monthly)
 		self.cp = float(car_payment)
 		self.cc = float(credit_card)
 		self.credp = float(credit_percent)
 		self.oth = float(other)
 		self.gp = float(gross_pay)
-		
+		self.p = float(payment)
 		
 	def debttoincome(self):
-		return (max((self.slm + self.cp + self.cc + self.oth), 1)/(self.gp / 12)) * 100
+		return (max((self.slm + self.cp + findPayment(self.cc, self.credp, .83333) + self.oth + self.p), 1)/(self.gp / 12)) * 100
--- mortgageMain.py
+++ mortgageMain.py
@@ -3,23 +3,6 @@ from subclasses import *
 from mortgage import *
 import pandas as pd
 import numpy as np
-
-
-dict_zip_to_county = pd.read_csv('zip_to_county.csv', header=1, index_col=0, squeeze=True).to_dict()
-
-dict_county_to_loan1 = pd.read_csv('county_1_2_3_4_unitloanlimits.csv', usecols=[0,1], index_col=0, header=1, squeeze=True).to_dict()
-dict_county_to_loan2 = pd.read_csv('county_1_2_3_4_unitloanlimits.csv', usecols=[0,2], index_col=0, header=1, squeeze=True).to_dict()
-dict_county_to_loan3 = pd.read_csv('county_1_2_3_4_unitloanlimits.csv', usecols=[0,3], index_col=0, header=1, squeeze=True).to_dict()
-dict_county_to_loan4 = pd.read_csv('county_1_2_3_4_unitloanlimits.csv', usecols=[0,4], index_col=0, header=1, squeeze=True).to_dict()
-
-for i in dict_county_to_loan1.keys():
-	dict_county_to_loan1[i] = dict_county_to_loan1[i].replace('$', '').replace(' ','').replace(',','')
-for i in dict_county_to_loan2.keys():
-	dict_county_to_loan2[i] = dict_county_to_loan2[i].replace('$', '').replace(' ','').replace(',','')
-for i in dict_county_to_loan3.keys():
-	dict_county_to_loan3[i] = dict_county_to_loan3[i].replace('$', '').replace(' ','').replace(',','')
-for i in dict_county_to_loan4.keys():
-	dict_county_to_loan4[i] = dict_county_to_loan4[i].replace('$', '').replace(' ','').replace(',','')
 	
 
 intro = input('Is this your first time using this script? ')
@@ -32,7 +15,7 @@ print('Now I will ask you to input some common information banks use to decide w
 preliminary = False
 
 while preliminary not in ['yes', 'YES', 'Yes', 'Yep', 'Y', 'YEs', 'yES', 'yeS', 'y']:
-	credit_score = int(input('What is your credit score? '))
+	credit_score = int(input('What is your credit score? (If you are married and including your spouse on the loan, include the lowest average FICO score between you and your spouse. ex: your three credit scores are 740, 720, and 700, and your spouse has three scores of 780, 760 and 740, you would enter 720 (740 + 720 +700)/3. HOWEVER, if you do put your spouse on the loan each time I ask for what you pay monthly for each category of debt, you must enter the total monthly you AND your spouse pay.'))
 	while credit_score <= 660:
 		print('Please enter a credit score that is higher than 660, you may want to raise your credit before seeking a mortgage.')
 		credit_score = int(input('What is your credit score? '))
@@ -75,62 +58,103 @@ else:
 	
 limit = (max_loan + down_payment) * 0.93
 
-print(f'The maximum loan in your area for the type of house you want to purchase is {max_loan}. With the ${down_payment} you have for a down payment, the priciest home that you can buy in this area is approximately {limit} when accounting for taxes and fees.')
+print(f'The maximum loan in your area for the type of house you want to purchase is {max_loan}. With the ${down_payment} you have for a down payment, the priciest home that you can buy in this area is approximately {limit} when accounting for taxes and fees.')	
+
+homeprice = int(input('What is the value of the home that you want to purchase? '))
+while homeprice >= limit or homeprice in ['quit', 'Quit', 'QUIT']:
+	print(f'That home is more than you can afford with your current downpayment. Banks will lend ${max_loan} for that zip code. Your down payment is {down_payment}, which means you can purchase a home under {limit} when you account for taxes and closing costs. Please note this is an estimate. You should also know, this does not take into account your credit score or your debt obligations which banks use to decide whether they will issue a loan. We will do that calculation in a bit.')
+	homeprice = int(input('For now let\'s get a smaller home value... what is the value of the home that you want to purchase? (type quit to stop'))
+
+
+usr_ltv = loan_to_value(homeprice, down_payment)
+Int = findinterestRate(credit_score)
+if usr_ltv <= .8500000:
+	mortI = MI(homeprice, down_payment, credit_score) + .0008
+elif usr_ltv <= .90000000:
+	mortI = MI(homeprice, down_payment, credit_score) + .0016
+elif usr_ltv <= .95000000:
+	mortI = MI(homeprice, down_payment, credit_score) + .0024
+else:
+	mortI = MI(homeprice, down_payment, credit_score) + .004
+	
+loan = homeprice * 1.07
+
+if mortI == None:
+	payment = findPayment(loan, Int, 30)
+else:
+	payment = findPayment(loan, Int, 30) + (mortI * loan)/12
 	
 print('Now let\'s run some numbers... Here\'s where I ask you about your debt obligations in order to see what you qualify for.')
 
 check = False
-check = input('Do you have student loans? (answer yes even if they are in deferment.) ')
+check = input('Do you have student loans? (answer yes even if they are in deferment.) (if including your spouse on the loan you must include his/her info)')
 if check in ['yes', 'YES', 'Yes', 'Yep', 'Y', 'YEs', 'yES', 'yeS', 'y']:
 	in_defer = input('Are your loans in deferment? (you are not actively making payments on them) ')
 	if in_defer in ['yes', 'YES', 'Yes', 'Yep', 'Y', 'YEs', 'yES', 'yeS', 'y']:
 		in_defer = True
-		student_loans_total = input('How much do you owe? ')
+		student_loans_total = input('How much do you owe? (if including your spouse on the loan you must include his/her info)')
 	else:
 		in_defer = False
 		try:
-			student_loans_monthly = input('How much do you pay each month? ')
+			student_loans_monthly = input('How much do you pay each month? (if including your spouse on the loan you must include his/her info)')
 		except ValueError:
 			print('Please enter a number with just numbers. no commas')
-			student_loans_monthly = input('How much do you pay each month? ')
+			student_loans_monthly = input('How much do you pay each month? (if including your spouse on the loan you must include his/her info)')
 	check = False
 else:
 	student_loans_monthly = 0
 	in_defer = False
 	check = False
-
-check = input('Do you have car payments? ')
+	
+check = input('Do you have car payments? (if including your spouse on the loan you must include his/her info)')
 if check in ['yes', 'YES', 'Yes', 'Yep', 'Y', 'YEs', 'yES', 'yeS', 'y']:
-	car_payment = input('What do you pay per month for your auto loans? ')
+	car_payment = input('What do you pay per month for your auto loans? (if including your spouse on the loan you must include his/her info)')
 	check = False
 else:
 	car_payment = 0
-
-check = input('Do you have credit card debt? ')
+	
+check = input('Do you have credit card debt? (if including your spouse on the loan you must include his/her info)')
 if check in ['yes', 'YES', 'Yes', 'Yep', 'Y', 'YEs', 'yES', 'yeS', 'y']:
 	credit_card = input('How much do you owe?')
-	credit_percent = input('What is the average percent of interest on your credit debt? ')
+	credit_percent = float(input('What is the average percent of interest on your credit debt? (if including your spouse on the loan you must include his/her info)'))
+	if credit_percent >= 0 and credit_percent <= 0.5:
+		credit_percent *= 100
+	elif credit_percent < 0 or credit_percent > 50:
+		credit_percent = float(input('Please enter either a decimal between .01 and .50 OR a number between 1 and 50. What is the average percent interest on your credit debt? (if including your spouse on the loan you must include his/her info)'))
 	check = False
 else:
-	credit_card = 0
+	credit_card = 1
+	credit_percent = 7.5
 	
-check = input('Do you have any other debt obligations? ')
+check = input('Do you have any other debt obligations? (if including your spouse on the loan you must include his/her info)')
 if check in ['yes', 'YES', 'Yes', 'Yep', 'Y', 'YEs', 'yES', 'yeS', 'y']:
-	other = input('How much do you pay per month for these obligations? ')
+	other = input('How much do you pay per month for these obligations? (if including your spouse on the loan you must include his/her info)')
 else:
 	other = 0
 	
-gross_pay = input('What is your annual salary before taxes? ')
+gross_pay = input('What is your annual salary before taxes? (if including your spouse on the loan you must include his/her info)')
 
 if in_defer == True:
-	usr_CD = calcDebt_defer(student_loans_total, car_payment, credit_card, other, gross_pay)
+	usr_CD = calcDebt_defer(student_loans_total, car_payment, credit_card, other, gross_pay, credit_percent, payment = 0)
 	debtratio = usr_CD.debttoincome()
+	usr_CDwM = calcDebt_defer(student_loans_total, car_payment, credit_card, other, gross_pay, credit_percent, payment = 0)
+	debtratiowM = usr_CDwM.debttoincome() 
 else:
-	usr_CD = calcDebt(student_loans_monthly, car_payment, credit_card, other, gross_pay)
+	usr_CD = calcDebt(student_loans_monthly, car_payment, credit_card, other, gross_pay, credit_percent, payment = 0)
 	debtratio = usr_CD.debttoincome()
+	usr_CDwM = calcDebt(student_loans_monthly, car_payment, credit_card, other, gross_pay, credit_percent, payment)
+	debtratiowM = usr_CDwM.debttoincome()
 
-print(f'Banks will take the total debt obligations you have each month and use that value as part of their decision making. They will use this information to determine how much money they will lend you. Your debt to income is {debtratio} before your loan is added. If you put less than 20 percent down, banks want to see your total debt to income, including your mortgage, be under 45 percent. If you put 20 percent or more down that ratio increases to 50 percent.')
 
+
+print(f'Banks will take the total debt obligations you have each month and use that value as part of their decision making. They will use this information to determine how much money they will lend you. Your debt to income is {debtratio} before your loan is added. If you put less than 20 percent down, banks want to see your total debt to income ratio, including your mortgage, be under 45 percent. If you put 20 percent or more down that ratio increases to 50 percent.')
+
+if debtratiowM <= 45:
+	print(f'Now that you want to add a mortgage payment your debt ratio is {debtratiowM}. Which means the bank will usually give you the loan. Your payment is {payment} with an insurance rate of {Int} and mortgage insurance of {mortI}')
+elif debtratiowM <= 50 and usr_ltv <= .80:
+	print(f'Now that you want to add a mortgage payment your debt ratio is {debtratiowM}. Which means the bank will usually give you the loan. Your payment is {payment} with an insurance rate of {Int} and mortgage insurance of {mortI}')
+else:
+	print(f'It looks like you do not qualify for this loan as your debt to income ratio ({debtratiowM}) is too high. You need to save a larger down payment or look for a cheaper house.')
 
 
 		
